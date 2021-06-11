2536b5f7369592a471e9150f61a0cd396d4c7b17
Leonor Figueiredo <leonorpatriciasf@hotmail.com>
2021-06-11 12:37:33 +0100

    NewWorld changes World

--- MyCaves/NewWorld.cs
+++ MyCaves/NewWorld.cs
@@ -7,10 +7,34 @@ namespace MyCaves
         private int x;
         private int y;
 
+        Random rnd = new Random();
+
         public NewWorld(int x, int y)
         {
             this.x = x;
             this.y = y;
+        }
+
+        public void Show_World()
+        {
+            int [,] world = new int [this.x, this.y];
+
+            for(int i = 0; i < this.x; i++)
+            {
+                for(int j = 0; j < this.y; j++)
+                {
+                    int t = rnd.Next(0, 2);
+                    if(t == 0)
+                    {
+                        Console.Write(".");
+                    }
+                    else if(t == 1)
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
--- MyCaves/Program.cs
+++ MyCaves/Program.cs
@@ -13,14 +13,22 @@ namespace MyCaves
             int x = int.Parse(args[0]);
             int y = int.Parse(args[1]);
             int passos = int.Parse(args[2]);
+            int repeticao = 0;
 
             if (n == 3)
             {
                 Console.WriteLine("Let's start");
                 World bigWorld = new World(x, y);
                 NewWorld newWorld = new NewWorld(x, y);
-                AuxWorld auxWorld = new AuxWorld(x, y);
+                AuxWorld auxWorld = new AuxWorld(x,y);
                 bigWorld.Show_World();
+
+                while (repeticao < passos)
+                {
+                    Console.WriteLine("");
+                    newWorld.Show_World();
+                    repeticao++;
+                }
             }
             else
             {
