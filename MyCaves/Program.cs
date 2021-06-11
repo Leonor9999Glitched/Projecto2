8950699bd47dbe1a2c6ef01912115c32b575cc62
Leonor Figueiredo <leonorpatriciasf@hotmail.com>
2021-06-11 10:56:32 +0100

    Programa detecta argumentos

---
+++ MyCaves/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyCaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = args.Length;
+           if (n == 3)
+            {
+                Console.WriteLine("Let's start");
+            }
+            else
+            {
+                Console.WriteLine("Only three arguments");
+            }
+
+        }
+    }
+}
