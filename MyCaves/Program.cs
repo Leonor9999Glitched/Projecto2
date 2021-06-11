946d56cd455a6e672c7ed72ade506dfcfb57797a
Leonor Figueiredo <leonorpatriciasf@hotmail.com>
2021-06-11 12:07:17 +0100

    Verificar limites

--- MyCaves/Program.cs
+++ MyCaves/Program.cs
@@ -13,15 +13,7 @@ namespace MyCaves
             int x = int.Parse(args[0]);
             int y = int.Parse(args[1]);
             int passos = int.Parse(args[2]);
-            //int repetição = 0;
 
-            //Arrays para o mundo
-            //int [,] world = new int [x,y];
-            //int [,] newWorld = new int [x,y];
-            //int [,] auxWorld = new int [x,y];
-
-            //Definir se é Rock ou Ground
-            Random rnd = new Random();
             if (n == 3)
             {
                 Console.WriteLine("Let's start");
--- MyCaves/World.cs
+++ MyCaves/World.cs
@@ -6,6 +6,8 @@ namespace MyCaves
     {
         private int x;
         private int y;
+        private int cantos_x;
+        private int cantos_y;
 
         Random rnd = new Random();
 
@@ -35,6 +37,25 @@ namespace MyCaves
                 }
                 Console.WriteLine("");
             }
+        }
+
+        public int Verificarcantos_x()
+        {
+            //Ver se o mundo não sai dos limites
+            //Se o x for inferior a zero
+            if(cantos_x < 0)
+            {
+                //Devolverá o valor do limite
+                cantos_x = this.x;
+            }
+
+            if(cantos_x > this.x)
+            {
+                //Se o valor for maior que o limite
+                //Devolverá o valor zero, o inicio
+                cantos_x = 0;
+            }
+            return cantos_x;
         }
 
     }
